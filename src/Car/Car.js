0dc4c02adcf299c20b7774b64846adb30851ae6d
Manux21 <Manux21@yandex.ru>
2022-07-14 14:47:54 +0300

    Created simple React component

---
+++ src/Car/Car.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+// function car() {
+//     return(
+//         <div>This is car component</div>
+//     )
+// }
+
+// const car = () => {
+//     return(
+//         <div>This is car component</div>
+//     )
+// }
+//
+// const car = () => <div>This is car component</div>
+// const car = () => (
+//     <div>This is car component
+//         <strong> test</strong>
+//     </div>
+// )
+
+// export default car
+
+// export default () => (
+//     <div>
+//         This is car component
+//         <strong> test</strong>
+//     </div>
+// )
+
+export default () => (
+    <div>
+        <p>This is car component</p>
+        <p><strong>{Math.random()}</strong></p>
+    </div>
+)
