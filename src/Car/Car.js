efc3f002282c65b5d57e3827b6f50c4b89f6e249
Manux21 <Manux21@yandex.ru>
2022-07-15 00:16:26 +0300

    Added dynamic React components

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -28,9 +28,26 @@ import React from 'react'
 //     </div>
 // )
 
-export default () => (
+// export default () => (
+//     <div>
+//         <h3>This is car component</h3>
+//         <p>Number: <strong>{Math.round(Math.random() * 100)}</strong></p>
+//     </div>
+// )
+
+// export default props => ( //можно не писать круглые скобочки у пропс если у нас всего лишь один параметр
+//     <div>
+//         <h3>Car name: {props.name}</h3>
+//         <p>Year: <strong>{props.year}</strong></p>
+//         {props.children}
+//     </div>
+// )
+
+
+export default props => ( //можно не писать круглые скобочки у пропс если у нас всего лишь один параметр
     <div>
-        <p>This is car component</p>
-        <p><strong>{Math.random()}</strong></p>
+        <h3>Car name: {props.name}</h3>
+        <p>Year: <strong>{props.year}</strong></p>
+        <button onClick={props.onChangeTitle}>Click</button>
     </div>
 )
